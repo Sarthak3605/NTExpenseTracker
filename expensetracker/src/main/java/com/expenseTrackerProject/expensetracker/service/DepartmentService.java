6fa9688ab792a5f3d516777ceaae7d6dfca750e6
Sarthak Jain <96368767+Sarthak3605@users.noreply.github.com>
2025-07-16 01:02:29 +0530

    Update DepartmentService.java

--- expensetracker/src/main/java/com/expenseTrackerProject/expensetracker/service/DepartmentService.java
+++ expensetracker/src/main/java/com/expenseTrackerProject/expensetracker/service/DepartmentService.java
@@ -10,7 +10,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DepartmentService {
     private final DepartmentRepository departmentRepository;
-
     // Add a new department
     public Department addDepartment(Department department) {
 		if (departmentRepository.existsByName(department.getName())) {
