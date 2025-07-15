a5332d4863354d5f670f88c8ab109c0c3639bfed
Sarthak Jain <96368767+Sarthak3605@users.noreply.github.com>
2025-07-16 01:08:31 +0530

    Update ExpenseRepository.java

--- expensetracker/src/main/java/com/expenseTrackerProject/expensetracker/repository/ExpenseRepository.java
+++ expensetracker/src/main/java/com/expenseTrackerProject/expensetracker/repository/ExpenseRepository.java
@@ -1,27 +1,27 @@
 package com.expenseTrackerProject.expensetracker.repository;
 
-import com.expenseTrackerProject.expensetracker.model.Expense;
-import com.expenseTrackerProject.expensetracker.model.ExpenseStatus;
-import com.expenseTrackerProject.expensetracker.model.Department;
-import com.expenseTrackerProject.expensetracker.model.User;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
+
+import com.expenseTrackerProject.expensetracker.model.Department;
+import com.expenseTrackerProject.expensetracker.model.Expense;
+import com.expenseTrackerProject.expensetracker.model.ExpenseStatus;
+import com.expenseTrackerProject.expensetracker.model.User;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
     //Find expenses by status (for finance team)
     List<Expense> findByStatus(ExpenseStatus status);
 
     //Find pending expenses for a manager's department
     List<Expense> findByStatusAndUser_Department(ExpenseStatus status, Department department);
 
-	@Query("SELECT e FROM Expense e WHERE e.status = 'PENDING' AND e.user.department = :department")
+	@Query("SELECT e FROM Expense e WHERE e.status = 'PENDING' AND e.user.department = :department") //department specific
    List<Expense> findPendingExpensesByDepartment(@Param("department") Department department);
 
    @Query("SELECT e FROM Expense e WHERE e.status = 'APPROVED'")
